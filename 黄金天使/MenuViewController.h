f981d04052d4af8a5e5010c027c7f2af8204a951
cartmanguo <nggch1987@163.com>
2014-08-04 23:27:22 +0800

    initial

---
+++ 黄金天使/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  黄金天使
+//
+//  Created by Cartman on 14-8-4.
+//  Copyright (c) 2014年 Cartman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
